26e484337d266606eab0e6230dd094d795cb3c7a
CourageousTern <81249961+CourageousTern@users.noreply.github.com>
2022-03-04 18:15:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ var getUserChoice = function (userInput) {
     if (userInput === `rock` || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
         return userInput;
     } else {
-        console.log('"You must enter "rock", "paper" or "scissors"');
+        console.log('You must enter "rock", "paper" or "scissors"');
     }
 };
 
 var getComputerChoice = function () {
-    var randomNumber = Math.floor(Math.random * 3)
+    var randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber === 0) {
-        return `rock`
+        return `rock`;
     } else if (randomNumber === 1) {
-        return `paper`
+        return `paper`;
     } else if (randomNumber === 2) {
-        return `scissors`
+        return `scissors`;
     }
 };
 
@@ -24,33 +24,36 @@ var determineWinner = function (userChoice, computerChoice) {
     if (userChoice === 'bomb') {
         return 'The User Won';
     }
-    if (userChoice = computerChoice) {
-        return (`The game is a tie`)
+    if (userChoice === computerChoice) {
+        return `The game is a tie`;
     }
     
     if (userChoice === `rock`) {
         if (computerChoice === `paper`) {
-            return `The Computer Won`
+            return `The Computer Won`;
         }
+
         if (computerChoice === `scissors`) {
-            return `The User Won`
+            return `The User Won`;
         }
     }
 
     if (userChoice === `paper`) {
         if (computerChoice === `scissors`) {
-            return `The Computer Won`
+            return `The Computer Won`;
         }
+
         if (computerChoice === `rock`) {
-            return `The User Won`
+            return `The User Won`;
         }
     }
     if (userChoice === `scissors`) {
         if (computerChoice === `rock`) {
-            return `The Computer Won`
+            return `The Computer Won`;
         }
+
         if (computerChoice === 'paper') {
-            return `The User Won`
+            return `The User Won`;
         }
     }
 };
